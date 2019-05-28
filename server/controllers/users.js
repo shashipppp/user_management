0479e528145791529b419e74845cda84c336978f
shashipppp <47203639+shashipppp@users.noreply.github.com>
2019-05-28 14:55:01 +0530

    Update users.js

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,11 +2,12 @@
 const User = require('../models/user');
 const mongoose = require('mongoose');
 const mongo = require('../router/mongo');
+const db = " ";
 
 
 exports.users_get_all = async(req,res,next)=>{
       
-   //await mongo.connect;
+   
   await  User.find().exec().then(docs=>{
         const response = {
             count: docs.length,
@@ -23,7 +24,7 @@ exports.users_get_all = async(req,res,next)=>{
 
         }
         
-        mongoose.connection.close('mongodb://shashipppp:shashi3333@ds019936.mlab.com:19936/user_management',(err)=>{ if(err){
+        mongoose.connection.close(db,(err)=>{ if(err){
             console.log('mongoErr'+err);
         }else{
             console.log('Successfully disconnected to the Mongosee DB: user_management');
