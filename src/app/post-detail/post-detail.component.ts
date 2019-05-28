acefdc99de9bb52da11647b84ddea3fee7a38533
shashipppp <47203639+shashipppp@users.noreply.github.com>
2019-05-28 15:01:14 +0530

    Update post-detail.component.ts

--- src/app/post-detail/post-detail.component.ts
+++ src/app/post-detail/post-detail.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges, ViewChild, ElementRef } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { UserService } from '../user.service';
-import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit,  OnChanges } from '@angular/core';
+
 
 @Component({
   selector: 'app-post-detail',
@@ -10,58 +8,21 @@ import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 })
 export class PostDetailComponent implements OnInit,OnChanges {
   
-  atitle:string = 'Hi buddy';
-  success = false;
-  Card_Form: FormGroup;
-  result:string;
+
   
-@ViewChild('card_image') card_image:ElementRef;
+
 
   constructor(private fb: FormBuilder, private userService:UserService) { }
 
   ngOnInit() {
-    console.log(this.atitle);
-   this.Card_Form = this.fb.group({
-     title:['',[Validators.required,Validators.minLength(4)]],
-     summary:['',[Validators.required]],
-     category: ['',[Validators.required]],
-     content: [''],
-     parental_view: [false,[Validators.required]],
-     approved:[false],
-     image: ['',[Validators.required]]
-   }) 
+   
   }
 
-  ngOnChanges(changes: SimpleChanges){
+  ngOnChanges(){
     
-    console.log(this.atitle);
+    
 
   }
 
-  onSubmit(){
-    const formValue = this.Card_Form.value;
-    console.log(this.card_image);
-    const image = this.card_image.nativeElement;
-    const ImageFile:File = image.files[0];
-    
-    const currentUser = JSON.parse(sessionStorage.getItem('myapp_user'));
-    const created_by = currentUser.USERID;
-
-    const formData = new FormData();
-    formData.append('title', formValue.title);
-    formData.append('summary',formValue.summary);
-    formData.append('content',formValue.content);
-    formData.append('category',formValue.category);
-    formData.append('parental_view',formValue.parental_view);
-    formData.append('approved',formValue.approved);
-    formData.append('image',ImageFile,ImageFile.name);
-    formData.append('created_by', created_by);
-    this.userService.userEdit(formData).subscribe((res:HttpResponse<any>)=>{
-      console.log(res);
-      this.success = true;
-    
-    },(err:HttpErrorResponse)=>{
-      console.log(err);
-    })
-  }
+ 
 }
